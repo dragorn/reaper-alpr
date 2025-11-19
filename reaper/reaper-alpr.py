fa524d34967b4e671674bb2220dde4b764f607d4
Mike Kershaw / Dragorn <dragorn@kismetwireless.net>
2025-11-19 14:56:10 -0500

    reorganize things add yaml config add dockerfile handle v1 protocol from older cams

--- reaper/reaper-alpr.py
+++ reaper/reaper-alpr.py
@@ -14,23 +14,20 @@ import os
 import socket
 import statistics
 import struct
+import yaml
 from mjpeg_streamer import MjpegServer, Stream
-
-alpr = ALPR(
-    detector_model="yolo-v9-t-384-license-plate-end2end",
-    ocr_model="global-plates-mobile-vit-v2-model",
-)
 
 # fps decimation (1 in X frames processed)
 fpsDecimation = 15
-reaperHost = "192.168.3.100"
-reaperPort = 2000
 
 # toggle between IR and color
 s_imgIR = True
 s_FrameNum = -1
 
-stream = Stream("reaper", size=(1640, 922), quality=50, fps=15)
+stream = Stream("reaper", size=(1640, 922), quality=95, fps=15)
+color_stream = Stream("reaper-color", size=(1640, 922), quality=95, fps=15)
+
+protocolVersion = 0
 
 def mark_image(img: np.ndarray, alpr_results: list[ALPRResult]) -> np.ndarray:
     for result in alpr_results:
@@ -104,9 +101,10 @@ def handle_image(image):
     stream.set_frame(cvmarked)
 
 
-def network_feed(host, port):
+def network_feed(config):
     client_socket = socket.socket()
-    client_socket.connect((host, port))
+    client_socket.connect((config['reaperalpr']['reaper']['host'],
+                           config['reaperalpr']['reaper']['port']))
 
     chunk = bytearray()
 
@@ -118,26 +116,54 @@ def network_feed(host, port):
         vals = struct.unpack('<I', chunk[:4])
 
         if vals[0] == 3001:
-            while len(chunk) < 40:
-                recv = client_socket.recv(1024*256)
-                chunk.extend(recv)
+            if protocolVersion == 1:
+                while len(chunk) < 24:
+                    recv = client_socket.recv(1024*256)
+                    chunk.extend(recv)
 
-            vals = struct.unpack('<IIIIIIIIII', chunk[:40])
+                vals = struct.unpack('<IIIIII', chunk[:24])
 
-            # frametype = vals[0]
-            # cameraid = vals[2]
-            # framenum = vals[8]
-            jpeglen = vals[9]
+                # frametype = vals[0]
+                # cameraid = vals[2]
+                # framenum = vals[8]
+                jpeglen = vals[5]
 
-            # print("IR frametype", frametype, "camera", cameraid, "frame", framenum, "jpeg", jpeglen)
-            while len(chunk) < 40 + jpeglen:
-                recv = client_socket.recv(jpeglen)
-                chunk.extend(recv)
+                # print("v1 3001", vals)
 
-            handle_image(chunk[40:40+jpeglen])
+                while len(chunk) < 24 + jpeglen:
+                    recv = client_socket.recv(jpeglen)
+                    chunk.extend(recv)
 
-            #print(chunk[40:48].hex(), "...", chunk[40+jpeglen-8:40+jpeglen].hex(), " | ", chunk[40+jpeglen:40+jpeglen+8].hex())
-            chunk = chunk[40+jpeglen:]
+                handle_image(chunk[24:24+jpeglen])
+
+                # v1 doesn't seem to include a color image
+                cvimage = cv2.imdecode(np.asarray(bytearray(chunk[24:24+jpeglen]), dtype=np.uint8), cv2.IMREAD_COLOR)
+                color_stream.set_frame(cvimage)
+
+
+                chunk = chunk[24+jpeglen:]
+
+            elif protocolVersion == 2:
+                while len(chunk) < 40:
+                    recv = client_socket.recv(1024*256)
+                    chunk.extend(recv)
+
+                vals = struct.unpack('<IIIIIIIIII', chunk[:40])
+
+                # frametype = vals[0]
+                # cameraid = vals[2]
+                # framenum = vals[8]
+                jpeglen = vals[9]
+
+                # print("v2 3001", vals)
+
+                while len(chunk) < 40 + jpeglen:
+                    recv = client_socket.recv(jpeglen)
+                    chunk.extend(recv)
+
+                handle_image(chunk[40:40+jpeglen])
+
+                chunk = chunk[40+jpeglen:]
 
         elif vals[0] == 1640:
             while len(chunk) < 28:
@@ -150,13 +176,14 @@ def network_feed(host, port):
             # cameraid = vals[2]
             jpeglen = vals[6]
 
-            # print("COLOR frametype", frametype, "camera", cameraid, "jpeg", jpeglen)
+            print("1640", vals)
 
             while len(chunk) < 28 + jpeglen:
                 recv = client_socket.recv(jpeglen)
                 chunk.extend(recv)
 
-            # print(chunk[28:36].hex(), "...", chunk[28+jpeglen-8:28+jpeglen].hex(), " | ", chunk[28+jpeglen:28+jpeglen+8].hex())
+            cvimage = cv2.imdecode(np.asarray(bytearray(chunk[28:28+jpeglen]), dtype=np.uint8), cv2.IMREAD_COLOR)
+            color_stream.set_frame(cvimage)
 
             chunk = chunk[28+jpeglen:]
 
@@ -181,19 +208,38 @@ def network_feed(host, port):
     client_socket.close()
 
 
-
 if __name__ == '__main__':
+    with open('/config/reaperalpr.yaml', 'r') as confy:
+        reaperConfig = yaml.safe_load(confy)
+
     if "REAPER" in os.environ:
         reaperHost = os.environ["REAPER"]
     else:
-        print("Missing REAPER variable")
-        exit(1)
+        reaperHost = reaperConfig['reaperalpr']['reaper']['host']
+
+    protocolVersion = reaperConfig['reaperalpr']['reaper']['protocol']
+
+    fpsDecimation = reaperConfig['reaperalpr']['processing']['frames']
+
 
     if "FPS" in os.environ:
         fpsDecimation = int(os.environ["FPS"])
 
-    server = MjpegServer("0.0.0.0", 8080)
-    server.add_stream(stream)
+    server = MjpegServer("0.0.0.0",
+                         reaperConfig['reaperalpr']['mjpeg']['mjpeg_port'])
+
+    if reaperConfig['reaperalpr']['mjpeg']['serve_mjpeg']:
+        server.add_stream(stream)
+
+    if reaperConfig['reaperalpr']['mjpeg']['serve_mjpeg_color']:
+        server.add_stream(color_stream)
+
     server.start()
 
-    network_feed(reaperHost, reaperPort)
+    alpr = ALPR(
+            detector_model=reaperConfig['reaperalpr']['alpr']['detector_model'],
+            ocr_model=reaperConfig['reaperalpr']['alpr']['ocr_model'],
+            )
+
+
+    network_feed(reaperConfig)
